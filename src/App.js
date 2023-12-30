32a94fa8f102ff6dfbaf40707c09e3abf8313e00
Sintu Kumar <kumarsintu499@gmail.com>
2023-12-31 01:01:32 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Container from './Components/Container'
 
 function App() {
   return (
     <div>
       <h1>Todo App</h1>
+      <Container/>
     </div>
   )
 }
--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Container() {
   return (
     <div>
-      
+      <h1>Container</h1>
     </div>
   )
 }
